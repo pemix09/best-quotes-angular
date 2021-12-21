bfab8936fa83a9be736c3f461dcf733ddc0c30c6
Przemysław Klejno <pemix09@onet.pl>
2021-12-21 11:32:37 +0100

    App almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { QUOTES } from './models/data-base';
+import { Quotation } from './models/quotation';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {author: '', quotation: '', votes: 0};
+
+  onSwichForm(){
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation(){
+    this.quotes.unshift(this.quotation);
+    this.quotation = {author: '', quotation: '', votes: 0};
+  }
+
+  addVote(quote: Quotation, value: number){
+    quote.votes += value;
+  }
+}
